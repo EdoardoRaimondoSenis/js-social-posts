43160aedde54353f7a2db1e53f4e34a3171db2f6
EdoardoRaimondoSenis <166326043+EdoardoRaimondoSenis@users.noreply.github.com>
2024-06-24 17:48:15 +0200

    Aggiustamento codice e prova like al click

--- main.js
+++ main.js
@@ -59,230 +59,60 @@ const posts = [
 let contpost = document.getElementById("container");
 
 
-posts.forEach((element, index) => {
-    if (index == 0) {
-        
-        let imgIcon = `<img class="profile-pic" src="${posts[0].author.image}">`;
-        
-        let metaIcon = `<div class="post-meta__icon">${imgIcon}</div>`;
-        
-        let metaAuthor = `<div class="post-meta__author">${posts[0].author.name}</div>`;
-        
-        let metaTime = `<div class="post-meta__time">${posts[0].created}</div>`;
-        
-        let metaData = `<div class="post-meta__data">${metaAuthor} ${metaTime}</div>`
-        
-        let postMeta = `<div class="post__meta">${metaIcon} ${metaData}</div>`;
-        
-        let postHeader = `<div class="post__header">${postMeta}</div>`;
+posts.forEach((element) => {
 
-        let postText = `<div class="post__text">${posts[0].content}</div>`;
-
-        let bigImg = `<img src="${posts[0].media}" alt=""></img>`;
-
-        let postImage = `<div class="post__image">${bigImg}</div>`;
-
-        let likeButton = `<a class="like-button  js-like-button" href="#" data-postid="1">
+        contpost.innerHTML += 
+        `<div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${element.author.image}" alt="Phil Mangione">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${element.author.name}</div>
+                        <div class="post-meta__time">${element.created}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${element.content}</div>
+            <div class="post__image">
+                <img src="${element.media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
                             <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                             <span class="like-button__label">Mi Piace</span>
-                        </a>`;
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>`
 
-        let cta = `<div class="likes__cta">${likeButton}</div>`;
+        posts.forEach((element) => {
 
-        let likeCounter = `Piace a <b id="like-counter-1" class="js-likes-counter">${posts[0].likes}</b> persone`;
-
-        let counter = `<div class="likes__counter">${likeCounter}</div>`;
-
-        let jsLikes = `<div class="likes js-likes">${cta} ${counter}</div>`;
-
-        let postFolder = `<div class="post__footer">${jsLikes}</div>`
+            let likeButton = document.querySelector(".like-button");
+            likeButton = element.likes;
+            let numberLikes = document.getElementById("like-counter-1");
         
-        let post = `<div class="post">${postHeader} ${postText} ${postImage} ${postFolder}</div>`;
-        contpost.innerHTML += post;
-    }
-
-    if (index == 1) {
+            likeButton.addEventListener("click",
+                function () {
+                    likeButton.classList.toggle("like-button--liked");
+                    numberLikes++;
+                    console.log(element);
         
-        let imgIcon = `<img class="profile-pic" src="${posts[1].author.image}">`;
-        
-        let metaIcon = `<div class="post-meta__icon">${imgIcon}</div>`;
-        
-        let metaAuthor = `<div class="post-meta__author">${posts[1].author.name}</div>`;
-        
-        let metaTime = `<div class="post-meta__time">${posts[1].created}</div>`;
-        
-        let metaData = `<div class="post-meta__data">${metaAuthor} ${metaTime}</div>`
-        
-        let postMeta = `<div class="post__meta">${metaIcon} ${metaData}</div>`;
-        
-        let postHeader = `<div class="post__header">${postMeta}</div>`;
-
-        let postText = `<div class="post__text">${posts[1].content}</div>`;
-
-        let bigImg = `<img src="${posts[1].media}" alt=""></img>`;
-
-        let postImage = `<div class="post__image">${bigImg}</div>`;
-
-        let likeButton = `<a class="like-button  js-like-button" href="#" data-postid="1">
-                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                            <span class="like-button__label">Mi Piace</span>
-                        </a>`;
-
-        let cta = `<div class="likes__cta">${likeButton}</div>`;
-
-        let likeCounter = `Piace a <b id="like-counter-1" class="js-likes-counter">${posts[1].likes}</b> persone`;
-
-        let counter = `<div class="likes__counter">${likeCounter}</div>`;
-
-        let jsLikes = `<div class="likes js-likes">${cta} ${counter}</div>`;
-
-        let postFolder = `<div class="post__footer">${jsLikes}</div>`
-        
-        let post = `<div class="post">${postHeader} ${postText} ${postImage} ${postFolder}</div>`;
-        contpost.innerHTML += post;
-    }
-
-    if (index == 2) {
-        
-        let imgIcon = `<img class="profile-pic" src="${posts[2].author.image}">`;
-        
-        let metaIcon = `<div class="post-meta__icon">${imgIcon}</div>`;
-        
-        let metaAuthor = `<div class="post-meta__author">${posts[2].author.name}</div>`;
-        
-        let metaTime = `<div class="post-meta__time">${posts[2].created}</div>`;
-        
-        let metaData = `<div class="post-meta__data">${metaAuthor} ${metaTime}</div>`
-        
-        let postMeta = `<div class="post__meta">${metaIcon} ${metaData}</div>`;
-        
-        let postHeader = `<div class="post__header">${postMeta}</div>`;
-
-        let postText = `<div class="post__text">${posts[2].content}</div>`;
-
-        let bigImg = `<img src="${posts[2].media}" alt=""></img>`;
-
-        let postImage = `<div class="post__image">${bigImg}</div>`;
-
-        let likeButton = `<a class="like-button  js-like-button" href="#" data-postid="1">
-                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                            <span class="like-button__label">Mi Piace</span>
-                        </a>`;
-
-        let cta = `<div class="likes__cta">${likeButton}</div>`;
-
-        let likeCounter = `Piace a <b id="like-counter-1" class="js-likes-counter">${posts[2].likes}</b> persone`;
-
-        let counter = `<div class="likes__counter">${likeCounter}</div>`;
-
-        let jsLikes = `<div class="likes js-likes">${cta} ${counter}</div>`;
-
-        let postFolder = `<div class="post__footer">${jsLikes}</div>`
-        
-        let post = `<div class="post">${postHeader} ${postText} ${postImage} ${postFolder}</div>`;
-        contpost.innerHTML += post;
-    }
-
-    if (index == 3) {
-        
-        let imgIcon = `<img class="profile-pic" src="${posts[3].author.image}">`;
-        
-        let metaIcon = `<div class="post-meta__icon">${imgIcon}</div>`;
-        
-        let metaAuthor = `<div class="post-meta__author">${posts[3].author.name}</div>`;
-        
-        let metaTime = `<div class="post-meta__time">${posts[3].created}</div>`;
-        
-        let metaData = `<div class="post-meta__data">${metaAuthor} ${metaTime}</div>`
-        
-        let postMeta = `<div class="post__meta">${metaIcon} ${metaData}</div>`;
-        
-        let postHeader = `<div class="post__header">${postMeta}</div>`;
-
-        let postText = `<div class="post__text">${posts[2].content}</div>`;
-
-        let bigImg = `<img src="${posts[3].media}" alt=""></img>`;
-
-        let postImage = `<div class="post__image">${bigImg}</div>`;
-
-        let likeButton = `<a class="like-button  js-like-button" href="#" data-postid="1">
-                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                            <span class="like-button__label">Mi Piace</span>
-                        </a>`;
-
-        let cta = `<div class="likes__cta">${likeButton}</div>`;
-
-        let likeCounter = `Piace a <b id="like-counter-1" class="js-likes-counter">${posts[3].likes}</b> persone`;
-
-        let counter = `<div class="likes__counter">${likeCounter}</div>`;
-
-        let jsLikes = `<div class="likes js-likes">${cta} ${counter}</div>`;
-
-        let postFolder = `<div class="post__footer">${jsLikes}</div>`
-        
-        let post = `<div class="post">${postHeader} ${postText} ${postImage} ${postFolder}</div>`;
-        contpost.innerHTML += post;
-    }
-
-    if (index == 4) {
-        
-        let imgIcon = `<img class="profile-pic" src="${posts[4].author.image}">`;
-        
-        let metaIcon = `<div class="post-meta__icon">${imgIcon}</div>`;
-        
-        let metaAuthor = `<div class="post-meta__author">${posts[4].author.name}</div>`;
-        
-        let metaTime = `<div class="post-meta__time">${posts[4].created}</div>`;
-        
-        let metaData = `<div class="post-meta__data">${metaAuthor} ${metaTime}</div>`
-        
-        let postMeta = `<div class="post__meta">${metaIcon} ${metaData}</div>`;
-        
-        let postHeader = `<div class="post__header">${postMeta}</div>`;
-
-        let postText = `<div class="post__text">${posts[4].content}</div>`;
-
-        let bigImg = `<img src="${posts[4].media}" alt=""></img>`;
-
-        let postImage = `<div class="post__image">${bigImg}</div>`;
-
-        let likeButton = document.createElement("a");
-        likeButton.classList.add("like-button", "js-like-button");
-        
-
-        let cta = `<div class="likes__cta">${likeButton}</div>`;
-
-        let likeCounter = `Piace a <b id="like-counter-1" class="js-likes-counter">${posts[4].likes}</b> persone`;
-
-        let counter = `<div class="likes__counter">${likeCounter}</div>`;
-
-        let jsLikes = `<div class="likes js-likes">${cta} ${counter}</div>`;
-
-        let postFolder = `<div class="post__footer">${jsLikes}</div>`
-        
-        let post = `<div class="post">${postHeader} ${postText} ${postImage} ${postFolder}</div>`;
-        contpost.innerHTML += post;
-
-        
-
-        likeButton.addEventListener("click",
-            function () {
-                likebutton.classList.toggle("like-button--liked");
-                
-                for (let i = 0; i < posts.length; i++) {
-                    const nuovolike = posts[i];
-                    console.log(nuovolike);
-
-                    let likeagg;
-                    nuovolike.likes = likeagg++;
-                    console.log(likeagg);
                 }
-            
-            
-            }
-        );
+            );
+        
+        
+        });
+        
+        
     }
-}
 );
 
 
